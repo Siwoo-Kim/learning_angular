1a79f49bfe70dbda4bb3218e809030079887926a
Siwoo-Kim <skim327@myseneca.ca>
2018-01-21 10:57:56 -0500

    Angular Learning 1-21

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import {HttpModule} from "@angular/http";
+import { HomeComponent } from './component/common/home/home.component';
+import {RouterModule} from "@angular/router";
+import {routes} from "./app.routes";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {
+  MatButtonModule, MatCardModule, MatInputModule, MatRadioModule, MatSnackBarModule,
+  MatToolbarModule
+} from "@angular/material";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    /* Angular Module */
+    BrowserModule,HttpModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    /* Angular Material & Animation Modeul */
+    MatToolbarModule,MatSnackBarModule,MatCardModule,MatInputModule,
+    MatRadioModule,MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
