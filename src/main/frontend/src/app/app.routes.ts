43214f6c7c29960dc22d3e48895734c390da4505
Siwoo-Kim <skim327@myseneca.ca>
2018-01-21 10:59:01 -0500

    Angular Learning 1-21

---
+++ src/main/frontend/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import {Routes} from "@angular/router";
+import {HomeComponent} from "./component/common/home/home.component";
+
+export const routes:Routes = [
+  {path:'',component:HomeComponent}
+]
