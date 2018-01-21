6c5e4df85e837218b6eecebacc7d2be1401b97ea
Siwoo-Kim <skim327@myseneca.ca>
2018-01-21 11:57:07 -0500

    Angular Learning 1-21

--- src/main/frontend/src/app/component/chapter-1-startpoint/chapter-1-startpoint.component.ts
+++ src/main/frontend/src/app/component/chapter-1-startpoint/chapter-1-startpoint.component.ts
@@ -9,10 +9,20 @@ export class Chapter1StartpointComponent implements OnInit {
   name:string;
   escapeHtml1:string;
   escapeHtml2:string;
+  escapeHtml3:string;
   constructor() {
     this.name = 'Angular';
     this.escapeHtml1 = "{{name}}";
     this.escapeHtml2 = "\n" +
+      "@Component({\n" +
+      "  selector: 'app-chapter-1-startpoint',\n" +
+      "  templateUrl: './chapter-1-startpoint.component.html',\n" +
+      "  styleUrls: ['./chapter-1-startpoint.component.css']\n" +
+      "})\n" +
+      "export class Chapter1StartpointComponent implements OnInit {\n" +
+      "  name:string;\n"+"}";
+
+    this.escapeHtml3 = "\n" +
       "      @NgModule({\n" +
       "      declarations: [\n" +
       "      AppComponent,\n" +
