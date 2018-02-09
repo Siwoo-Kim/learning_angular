18895f48a14114bda6fe8b027d713cebf2f29efc
Siwoo-Kim <skim327@myseneca.ca>
2018-02-08 22:51:56 -0500

    version 1-21

--- src/main/frontend/src/app/project2/recipe-project/components/header/header.component.ts
+++ src/main/frontend/src/app/project2/recipe-project/components/header/header.component.ts
@@ -2,7 +2,14 @@ import {Component} from "@angular/core";
 
 @Component({
   selector : 'recipe-header',
-  templateUrl : './header.component.html'
+  templateUrl : './header.component.html',
+  styles : [`
+    .nav-tabs {
+      background-color: cornsilk;
+      font-size: 1.1em;
+      font-weight: bold;
+    }
+  `]
 })
 export class HeaderComponent{
 
